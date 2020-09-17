dda3560ae4262eec9ef789629638a7b651f265f3
dongjiuzhu <dongjiuzhu1@xiaomi.com>
2020-09-17 16:38:48 +0800

    frameworks/topics: init version sensor topics
    Signed-off-by: dongjiuzhu <dongjiuzhu1@xiaomi.com>

---
+++ src/sensor/co2.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <uORB/common/log.h>
+#include <sensor/co2.h>
+
+#ifdef CONFIG_DEBUG_FEATURES
+static void print_sensor_co2_message(const void* buffer)
+{
+    const struct sensor_co2* message = (const struct sensor_co2*)buffer;
+
+    uorbinfo_raw(" sensor_co2\n");
+
+    const orb_abstime now = orb_absolute_time();
+
+    if (message->timestamp != 0) {
+        uorbinfo_raw("\ttimestamp: %" PRIu64 "  (%.6f seconds ago)\n", message->timestamp, (now - message->timestamp) / 1e6);
+    } else {
+        uorbinfo_raw("\n");
+    }
+
+    uorbinfo_raw("\tco2: %.4f\n", message->co2);
+}
+
+ORB_DEFINE(sensor_co2, struct sensor_co2, print_sensor_co2_message);
+#else
+ORB_DEFINE(sensor_co2, struct sensor_co2, NULL);
+#endif
