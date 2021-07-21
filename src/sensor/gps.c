59cd9e731f2c8f537c24b6ecdc154d7a6ea67eb3
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-07-21 15:52:27 +0800

    gps: modify gps topic stucture to support more info
    Change-Id: I689f16ab95a8bffc1f62551c0550fe3045d9f22f

--- src/sensor/gps.c
+++ src/sensor/gps.c
@@ -14,23 +14,44 @@
  * limitations under the License.
  */
 
-#include <uORB/common/log.h>
 #include <sensor/gps.h>
+#include <uORB/common/log.h>
 #include <uORBTopics.h>
 
 #ifdef CONFIG_DEBUG_FEATURES
-static void print_sensor_gps_message(const struct orb_metadata *meta, const void* buffer)
+static void print_sensor_gps_message(const struct orb_metadata* meta, const void* buffer)
 {
     const struct sensor_gps* message = (const struct sensor_gps*)buffer;
+    const orb_abstime now = orb_absolute_time();
 
-    uorbinfo_raw("%s:\tyear: %d month: %d day: %d hour: %d min: %d sec: %d msec: %d",
-                  meta->o_name, message->year, message->month, message->day, message->hour, message->min, message->sec, message->msec);
+    uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago) time_utc: %" PRIu64 " latitude: %.4f longitude: %.4f",
+        meta->o_name, message->timestamp, now - message->timestamp, message->time_utc, message->latitude, message->longitude);
 
-    uorbinfo_raw("%s:\tyaw: %.4f height: %.4f speed: %.4f latitude: %.4f longitude: %.4f",
-                  meta->o_name, message->yaw, message->height, message->speed, message->latitude, message->longitude);
+    uorbinfo_raw("%s:\taltitude: %.4f altitude_ellipsoid: %.4f ground_speed: %.4f course: %.4f",
+        meta->o_name, message->altitude, message->altitude_ellipsoid, message->ground_speed, message->course);
+
+    uorbinfo_raw("%s:\teph: %.4f epv: %.4f hdop: %.4f vdop: %.4f",
+        meta->o_name, message->eph, message->epv, message->hdop, message->vdop);
+}
+
+static void print_sensor_gps_satellite_message(const struct orb_metadata* meta, const void* buffer)
+{
+    const struct sensor_gps_satellite* message = (const struct sensor_gps_satellite*)buffer;
+    const orb_abstime now = orb_absolute_time();
+    int i;
+
+    uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago)",
+        meta->o_name, message->timestamp, now - message->timestamp);
+
+    for (i = 0; i < message->count; i++) {
+        uorbinfo_raw("%s:\tnumber:%d svid: %d .elevation: %d azimuth: %d snr: %d",
+            meta->o_name, i, message->info[i].svid, message->info[i].elevation, message->info[i].azimuth, message->info[i].snr);
+    }
 }
 
 ORB_DEFINE(sensor_gps, struct sensor_gps, print_sensor_gps_message, sensor_gps);
+ORB_DEFINE(sensor_gps_satellite, struct sensor_gps_satellite, print_sensor_gps_satellite_message, sensor_gps_satellite);
 #else
 ORB_DEFINE(sensor_gps, struct sensor_gps, NULL, sensor_gps);
+ORB_DEFINE(sensor_gps_satellite, struct sensor_gps_satellite, NULL, sensor_gps_satellite);
 #endif
