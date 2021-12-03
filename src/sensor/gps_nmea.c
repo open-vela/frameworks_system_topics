2249f03264d7f9285bf1eb8f5e621ecbc8024e4f
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2021-12-03 14:10:04 +0800

    topic/gps_nmea: add gps_nmea topic
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

---
+++ src/sensor/gps_nmea.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <uORB/common/log.h>
+#include <sensor/gps_nmea.h>
+#include <uORBTopics.h>
+
+#ifdef CONFIG_DEBUG_FEATURES
+static void print_gps_nmea_message(const struct orb_metadata *meta, const void* buffer)
+{
+    const struct gps_nmea* message = (const struct gps_nmea*)buffer;
+    const orb_abstime now = orb_absolute_time();
+
+    uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago) nmea: %s",
+                  meta->o_name, message->timestamp, now - message->timestamp, message->nmea);
+}
+
+ORB_DEFINE(gps_nmea, struct gps_nmea, print_gps_nmea_message, gps_nmea);
+#else
+ORB_DEFINE(gps_nmea, struct gps_nmea, NULL, gps_nmea);
+#endif
