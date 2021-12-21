d1e68d51f0104cb009972e2196173117a2d783dd
xiatian6 <xiatian6@xiaomi.com>
2021-12-21 14:11:46 +0800

    feature: sensor: Modify the data structure of mag
    Change-Id: I6ac70b5d8f7b015e619dbad64f93280f04540cd6

--- include/sensor/mag.h
+++ include/sensor/mag.h
@@ -25,6 +25,7 @@ struct sensor_mag {
     float y; /* Axis Y in Gauss or micro Tesla (uT) */
     float z; /* Axis Z in Gauss or micro Tesla (uT) */
     float temperature; /* Temperature in degrees celsius */
+    int32_t status; /* Status of calibration */
 };
 
 /* register this as object request broker structure */
