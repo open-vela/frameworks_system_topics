c896cd3775824f02320c4bfffe7c65590a614b67
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-09-11 12:35:47 +0800

    frameworks/topics: support charger control by topic
    The limiting level of charger current level, range [0, 4],
    -1 is stop limiting current. The specific current value
    needs to refer to the configuration of the chargerd service.
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

---
+++ include/system/charger_control.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2024 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __TOPICS_INCLUDE_SYSTEM_CHARGER_CONTROL_H
+#define __TOPICS_INCLUDE_SYSTEM_CHARGER_CONTROL_H
+
+#include <uORB/uORB.h>
+
+struct charger_control {
+    uint64_t timestamp; /* Units is microseconds */
+
+    /* The limiting level of charger current level,
+     * range [0, 4], -1 is stop limiting current. The specific current value
+     * needs to refer to the configuration of the chargerd service.
+     */
+
+    int32_t curr_limit_level;
+};
+
+/* register this as object request broker structure */
+
+ORB_DECLARE(charger_control);
+#endif
