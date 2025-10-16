7b6a16a4ddd654e88bc36c199ff55e5149efa810
liubingyan <liubingyan@xiaomi.com>
2025-10-16 16:20:43 +0800

    feat(algo) : update algo_heartrate.h
    1.mv algo hr instance definations into algo_heartrate.h
    2.rename HR_INSTANCE_CONTINUOUS as HR_INSTANCE_SPORT
    
    Signed-off-by: liubingyan <liubingyan@xiaomi.com>

--- include/fitness/algo_heartrate.h
+++ include/fitness/algo_heartrate.h
@@ -18,12 +18,20 @@
 
 #include <uORB/uORB.h>
 
+#define HR_INSTANCE_MANUAL 0 // 手动测量
+#define HR_INSTANCE_LAST 1 // 最近一次测量
+#define HR_INSTANCE_UNCONSCIOUS 2 // 后台心率测量
+#define HR_INSTANCE_SPORT 3 // 运动心率
+#define HR_INSTANCE_MONITOR 4 // 异常心率监测
+#define HR_INSTANCE_RECOVER 5 // 恢复心率
+#define HR_INSTANCE_MAX_CNT 6 // 心率topic实例数量
+
 struct algo_heartrate {
-  uint64_t timestamp_us; // topic发布时间戳
-  uint32_t bpm;          // 心率值bpm
-  int heartrate_section;
-  int heartrate_section_max;
-  int heartrate_section_min;
+    uint64_t timestamp_us; // topic发布时间戳
+    uint32_t bpm; // 心率值bpm
+    int heartrate_section;
+    int heartrate_section_max;
+    int heartrate_section_min;
 };
 
 ORB_DECLARE(algo_heartrate);
